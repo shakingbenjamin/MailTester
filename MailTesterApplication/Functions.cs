4aa17da5eaca7679183b63f40493922f8fef8a79
shakingbenjamin <travesben1@gmail.com>
2019-02-14 18:05:25 +0000

    initial commit of mail tester

---
+++ MailTesterApplication/Functions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailTestApplication
+{
+    public class MailTester
+    {
+        public static string BodyBuilder(string server, int port, string to, string from)
+        {
+            var result = "";
+            try
+            {
+                var enl = "</br>";
+
+                var s = new MailStatus
+                {
+                    IPAddress = Dns.GetHostName(),
+                    MailServer = server,
+                    PortNumber = port,
+                    ToAddress = to,
+                    FromAddress = from,
+                    TimeSent = DateTime.Now.ToString()
+                };
+
+                result += $"<strong>Test Information</strong>{enl}{enl} Host/IP Address: {s.IPAddress}{enl} Mail Server: {s.MailServer}{enl}" +
+                    $"Port: {s.PortNumber}{enl}To: {s.ToAddress}{enl} From: {s.FromAddress}{enl} Sent At: {s.TimeSent}";
+            }
+            catch(Exception ex)
+            {
+                
+            }
+                
+            return result;
+        }
+
+        public static void SendEmail(string server,string port, string to, string from)
+        {
+            try
+            {
+                var _port = Convert.ToInt32(port);
+                string _body = BodyBuilder(server, _port, to, from);
+                string _subject = "Mail Services Test";
+
+                using (SmtpClient msgClient = new SmtpClient())
+                {
+                    msgClient.EnableSsl = false;
+                    msgClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    msgClient.UseDefaultCredentials = false;
+                    msgClient.Credentials = new NetworkCredential
+                    {
+                        // add necessary credentials
+                    };
+                    msgClient.Host = server; //"webmail.domain.com";
+                    msgClient.Port = _port; //25 by default;
+
+                    using (MailMessage msg = new MailMessage())
+                    {
+                        msg.IsBodyHtml = true;
+                        msg.To.Add(to);
+                        msg.From = new MailAddress(from);
+                        msg.Subject = _subject;
+                        msg.Body = _body;
+                        msgClient.Send(msg);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                // error logging
+            }
+        }
+    }
+}
---
+++ MailTesterApplication/Models.cs
@@ -0,0 +1,12 @@
+﻿namespace MailTestApplication
+{
+    public class MailStatus
+    {
+        public string IPAddress { get; set; }
+        public string ToAddress { get; set; }
+        public string FromAddress { get; set; }
+        public string MailServer { get; set; }
+        public int PortNumber { get; set; }
+        public string TimeSent { get; set; }
+    }
+}
